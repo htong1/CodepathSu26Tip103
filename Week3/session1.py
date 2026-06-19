6573e92858ebf1a8156ee13a4101e6e1725efe45
Hillary Tong <hztong@college.harvard.edu>
2026-06-19 10:37:35 -0400

    7.2 problem set 1 completed

---
+++ Week3/session1.py
@@ -0,0 +1,10 @@
+'''
+Problem 1: Arrange Guest Arrival Order
+Approach:
+'''
+
+def arrange_guest_arrival_order(arrival_pattern):
+  pass
+
+print(arrange_guest_arrival_order("IIIDIDDD"))  
+print(arrange_guest_arrival_order("DDD"))  
